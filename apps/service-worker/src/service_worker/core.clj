33374bc3d2844e6c7e3e30e309fa3f27a39c4c15
Arthur Dos Santos Almeida <arthur.almeida@g.globo>
2025-08-06 12:22:01 -0300

    fix: alterando worker para rodar de 5 em 5 segundos

--- apps/service-worker/src/service_worker/core.clj
+++ apps/service-worker/src/service_worker/core.clj
@@ -13,10 +13,10 @@
       (try
         (println "Starting pendents payments processing at " (.toString (java.time.Instant/now)))
         (service/process-pendents-payments)
-        (Thread/sleep 2000)
+        (Thread/sleep 5000)
         (catch Exception e
           (println "Error processing pendents payments:" (.getMessage e))
-          (Thread/sleep 2000))))))
+          (Thread/sleep 5000))))))
 
 (defn -main [& args]
   (http/start (http/create-server service-map))
